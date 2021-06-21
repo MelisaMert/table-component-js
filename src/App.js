fd7613ad1040fcfc41bce1f938de959cf9a8daa7
MelisaMert <melisamert024@gmail.com>
2021-06-21 23:24:58 +0300

    EditableTable Component

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import {EditableTable} from './table/EditableTable';
+import {TableOperation} from './table/Table';
+
+
+function App() {
+  return (
+    <>
+    <TableOperation />
+    <EditableTable />
+    </>
+  );
+}
+
+export default App;
